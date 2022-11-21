aebad23bc683c7b421c8594e25e7f7c3660eabf4
michaelenoma <enomamichael@gmail.com>
2022-11-21 22:48:02 +0100

    Ask permission on start

--- app/src/main/java/com/example/blechatcompose/MainActivity.kt
+++ app/src/main/java/com/example/blechatcompose/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.blechatcompose
 
+import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +12,11 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.blechatcompose.ui.theme.BLeChatComposeTheme
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.MultiplePermissionsReport
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -26,6 +32,30 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+        Dexter.withContext(this)
+            .withPermissions(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            )
+            .withListener(object : MultiplePermissionsListener {
+                override fun onPermissionsChecked(report: MultiplePermissionsReport) {
+                    ChatServer.startServer(application, activity = this@MainActivity)
+                    viewModel.startScan()
+                }
+
+                override fun onPermissionRationaleShouldBeShown(
+                    permissions: List<PermissionRequest?>?,
+                    token: PermissionToken?
+                ) {
+
+                }
+            })
+            .check()
     }
 }
 
