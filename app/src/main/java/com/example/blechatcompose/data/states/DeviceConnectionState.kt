617b04dae50cab73efc30b1187b674b4298d8959
michaelenoma <enomamichael@gmail.com>
2022-11-23 23:48:02 +0100

    Connection states and viewmodels

---
+++ app/src/main/java/com/example/blechatcompose/data/states/DeviceConnectionState.kt
@@ -0,0 +1,8 @@
+package com.example.blechatcompose.data.states
+
+import android.bluetooth.BluetoothDevice
+
+sealed class DeviceConnectionState {
+    class Connected(val device: BluetoothDevice) : DeviceConnectionState()
+    object Disconnected : DeviceConnectionState()
+}
---
+++ app/src/main/java/com/example/blechatcompose/utils/Constants.kt
@@ -0,0 +1,7 @@
+package com.example.blechatcompose.utils
+
+import java.util.*
+
+val SERVICE_UUID: UUID = UUID.fromString("0000b81d-0000-1000-8000-00805f9b34fb")
+val MESSAGE_UUID: UUID = UUID.fromString("7db3e235-3608-41f3-a03c-955fcbd2ea4b")
+
